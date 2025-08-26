31571b750d872dccba18f652b4531588d33d40d2
Leandro Santos <leandroleh07@gmail.com>
2025-08-26 10:52:19 -0300

    Commit Contador Project

---
+++ sintaxe/DesafioControleFluxo/Contador.java
@@ -0,0 +1,37 @@
+package sintaxe.DesafioControleFluxo;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (Exception e) {
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois) throws Exception {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm < parametroDois) {
+            int contagem = parametroDois - parametroUm;
+            //realizar o for para imprimir os números com base na variável contagem
+            for(int cont = 1; cont<=contagem; cont++) {
+                System.out.print(cont+" ");
+            }
+        } else {
+            throw new Exception("Erro Parâmetros");
+        }
+
+
+    }
+}
